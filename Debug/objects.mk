882131e084836a40f0a72e296e305a54ffdd4ce9
Chris Roadfeldt <chris@roadfeldt.com>
2017-04-26 04:30:11 +0000

    Repo clean up.

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_core -lboost_system -lboost_filesystem -lyaml-cpp -lntcore -lcudpp -lsl_zed -lsl_core -lpthread -lopencv_imgproc -lopencv_highgui
-
